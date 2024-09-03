540a6a893766c71790f220434dab849dfa1e8c54
MaFlet <mfletcher2808@hotmail.com>
2024-09-03 22:50:40 +1000

    3-print_number.py

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -4,6 +4,8 @@ This script prints the integer stored in the variable number,
 followed by batttery street, followed by a new line
 """
 # pylint: disable=invalid-name
-number = 98 or 333
-number = -98 or 0
+number = 98
+positive_number = 333
+negative_number = -98
+zero = 0
 print(f"{number} Battery street")
