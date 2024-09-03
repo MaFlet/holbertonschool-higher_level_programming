7f39086dbe43021ab88ac12a81bd3871af952735
MaFlet <mfletcher2808@hotmail.com>
2024-09-03 23:30:16 +1000

    4-print_float.py

---
+++ python-hello_world/4-print_float.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This script print the float stored in variable number
+with a precision of 2 digits
+"""
+# pylint: disable=invalid-name
+number = 3.14159
+print(f"Float: {number:.2f}")
