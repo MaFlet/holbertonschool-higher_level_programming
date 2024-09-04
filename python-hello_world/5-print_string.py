9881c58f8889cc5212aded4192838f04b62b6261
MaFlet <mfletcher2808@hotmail.com>
2024-09-04 20:13:39 +1000

    5-print_string.py

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-This script print 3 times a string stored in variable str,
-followed by its first 9 characters
-"""
+"""This script print 3 times a string"""
 # pylint: disable=invalid-name
 my_string = "Holberton School"
 print(my_string * 3 + my_string[:9])
