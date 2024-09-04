3d5320e79401e5f5ceb782cd0e0bfc949197dcab
MaFlet <mfletcher2808@hotmail.com>
2024-09-04 23:08:28 +1000

    8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""The Script prints the string desired output""" # pylint: disable=invalid-name
+# str = "Python is an interpreted, interactive, object-oriented programming\
+# language that combines remarkable power with very clear syntax"
+print("object-oriented programming with Python")
