fa92aa59d7d251c1a586223a5d75649fc79e00a3
MaFlet <mfletcher2808@hotmail.com>
2024-09-04 21:31:17 +1000

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """This script concates str1 and str2""" # pylint: disable=invalid-name
 str1, str2 = "Holberton", "School"
-str1, str2 = "Battery", "Street"
-print(f"Welcome to {'Holberton' + ' ' + 'School'}!\nWelcome to {'Battery' + ' ' + 'Street'}!")
+print(f"Welcome to {str1} {str2}!")
+print(f"Welcome to {'Battery' + ' ' + 'Street'}!")
