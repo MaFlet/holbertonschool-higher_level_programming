32fcfe24f0ce67110234c693947f015f30ebbddc
MaFlet <mfletcher2808@hotmail.com>
2024-09-04 21:49:43 +1000

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script for concation of str1 and str2"""
-# pylint: disable=invalid-name
-str1, str2 = "Holberton", "School"
-print("Welcome to {}!".format(str1 + " " + str2))
+str1 = "Holberton"
+str2 = "School"
+str1 += (" " + str2)
+print("Welcome to {}!".format(str1))
