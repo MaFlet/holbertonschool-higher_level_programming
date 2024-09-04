b706b190d5dba6320c1923572d3cd1a2b0f6c987
MaFlet <mfletcher2808@hotmail.com>
2024-09-04 20:42:42 +1000

    6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""This script concates str1 and str2"""
-# pylint: disable=invalid-name
-str1 = "Holberton"
-str2 = "School"
+"""This script concates str1 and str2""" # pylint: disable=invalid-name
+str1, str2 = "Holberton", "School"
 str3 = str1 + " " + str2
 print(f"Welcome to {str3}!")
