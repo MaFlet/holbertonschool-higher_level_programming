86114263c13f50cbb726e3796431a1cbf151817a
MaFlet <mfletcher2808@hotmail.com>
2024-09-04 20:18:17 +1000

    5-print_string.py

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -2,4 +2,4 @@
 """This script print 3 times a string"""
 # pylint: disable=invalid-name
 my_string = "Holberton School"
-print(my_string * 3 + my_string[:9])
+print(my_string * 3 + "\n" + my_string[:9])
