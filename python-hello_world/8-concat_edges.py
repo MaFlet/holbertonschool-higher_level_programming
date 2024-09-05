9b92245ab62176cf946d1c0173b08f30d8878080
MaFlet <mfletcher2808@hotmail.com>
2024-09-05 21:00:35 +1000

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-my_string = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-my_string = my_string[39:66] + my_string[106:112] + my_string[:6]
-print(my_string)
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
