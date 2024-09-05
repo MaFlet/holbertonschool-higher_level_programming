b8626bfff6f985c57999660cda3bab6e6cee8604
MaFlet <mfletcher2808@hotmail.com>
2024-09-05 22:50:50 +1000

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ each time it is executed
 # pylint: disable=invalid-name
 import random
 number = random.randint(-10000, 10000)
-last_digit = random.randint(0, 9)
+last_digit = abs(number) % 10
 if last_digit > 5:
     print("Last digit of", number, "is", last_digit, "and is greater than 5")
 elif last_digit < 6 and last_digit != 0:
