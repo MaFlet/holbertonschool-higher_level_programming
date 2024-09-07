199e701ec00a87cdcbcc9586aa0435afeb9fd9ef
MaFlet <mfletcher2808@hotmail.com>
2024-09-07 17:27:37 +1000

    8-uppercase.py

--- python-if_else_loops_functions/8-main.py
+++ python-if_else_loops_functions/8-main.py
@@ -3,6 +3,7 @@
 This script tests if strings are uppercase
 """
 # pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
 uppercase = __import__('8-uppercase').uppercase
 
 uppercase("best")
