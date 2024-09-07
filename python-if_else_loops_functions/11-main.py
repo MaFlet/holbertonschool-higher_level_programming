f9db634f7c5836c7692a0ab9e8a18f4d5d212a4d
MaFlet <mfletcher2808@hotmail.com>
2024-09-07 22:29:59 +1000

    11-pow.py

---
+++ python-if_else_loops_functions/11-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This script calculate a to the power of b
+"""
+# pylint: disable=invalid-name
+pow = __import__('11-pow').pow
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This script calculate a to the power of b
+"""
+# pylint: disable=invalid-name
+
+
+def pow(a, b):
+    """
+    Function define as computing a to the power
+    of b
+    """
+    return a ^ b
