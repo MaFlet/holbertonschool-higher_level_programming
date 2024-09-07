f16fcfe8bb0a6672e29e24cc41691339d09682d8
MaFlet <mfletcher2808@hotmail.com>
2024-09-07 16:27:45 +1000

    main and 8-uppercase.py

---
+++ python-if_else_loops_functions/8-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This script tests if strings are uppercase
+"""
+# pylint: disable=invalid-name
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script prints a string in uppercase followed by
+a new line
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+
+
+def uppercase(my_string):
+    """
+    Function that print uppercase characters
+    """
+    for letter in my_string:
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end="")
+    print()
