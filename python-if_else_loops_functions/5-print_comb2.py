451e4b09d6fd428e7e8ba82de68be2c3c0392d9e
MaFlet <mfletcher2808@hotmail.com>
2024-09-07 13:56:46 +1000

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -5,7 +5,4 @@ This script that prints numbers from 0 to 99
 # pylint: disable=invalid-name
 # pylint: disable=consider-using-f-string
 for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02d}".format(number), end=", ")
+    print("{:02d}".format(number), end=", " if number < 99 else "\n")
