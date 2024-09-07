d74d30e3f146a962fe9d6e7a78ebf5d3805994d3
MaFlet <mfletcher2808@hotmail.com>
2024-09-08 09:45:57 +1000

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,9 +4,10 @@ Script tester for printing addition results
 """
 # pylint: disable=invalid-name
 # pylint: disable=consider-using-f-string
-add = __import__('add_0').add
+if __name__ == "__main__":
+    add = __import__('add_0').add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
