66f0824ac6d39c07f9a66613d4f8c6303c94e059
MaFlet <mfletcher2808@hotmail.com>
2024-09-08 09:42:52 +1000

    task 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Script tester for printing addition results
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+add = __import__('add_0').add
+
+a = 1
+b = 2
+
+print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ python-import_modules/add_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script prints and adds 2 integers
+"""
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return a + b
