0998e2659e5b5de35419ab05a5c480031c2fc8dd
MaFlet <mfletcher2808@hotmail.com>
2024-09-07 20:36:06 +1000

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -11,5 +11,10 @@ def uppercase(my_string):
     """
     Function that prints uppercase characters.
     """
-    print('{}'.format(''.join(chr(ord(i) - 32)
-                              if 'a' <= i <= 'z' else i for i in my_string)))
+    new_string = ''
+    for i in my_string:
+        if 'a' <= i <= 'z':
+            new_string += chr(ord(i) - 32)
+        else:
+            new_string += i
+    print(f'{new_string}')
