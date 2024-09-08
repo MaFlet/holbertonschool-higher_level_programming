2dcaeb6e713b05bc54431b443e0765f50eb09e14
MaFlet <mfletcher2808@hotmail.com>
2024-09-08 11:40:48 +1000

    Task 5

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This script imports the variable "a" from file
+variable_load_5.py and prints its value
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+
+if __name__ == "__main__":
+    from variable_load_5 import a
+    print(a)
---
+++ python-import_modules/variable_load_5.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This script will be imported to file
+variable_load_5.y that can prints its value
+"""
+a = 98
+"""Simple variable
+"""
