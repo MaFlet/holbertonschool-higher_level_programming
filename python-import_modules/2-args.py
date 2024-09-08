0ff8933f91ea78c4f2f58e25e9c20b071e9832c2
MaFlet <mfletcher2808@hotmail.com>
2024-09-08 11:11:16 +1000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,13 +8,13 @@ its arguments
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:] #Excludes the first element, script name itself
-    num_args = len(argv) #number of arguments is determine by len
+    argv = sys.argv[1:]  # Excludes the first element, script name itself
+    num_args = len(argv)  # number of arguments is determine by len
 
     if num_args == 0:
-        print("0 arguments.") #If there are no arguments
+        print("0 arguments.")  # If there are no arguments
     elif num_args == 1:
-        print("1 argument:") #print excatly one argument
+        print("1 argument:")  # print excatly one argument
         print("1: {}". format(argv[0]))
     else:
         print("{} arguments:".format(num_args))
