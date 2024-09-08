26fbbb835ee43f9147982dbc14f5d3664fedf2d8
MaFlet <mfletcher2808@hotmail.com>
2024-09-08 20:03:25 +1000

    Task 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -10,4 +10,9 @@ def pow(a, b):
     Function define as computing a to the power
     of b
     """
-    return a ^ b
+    result = 1
+    for _ in range(abs(b)):
+        result *= a
+    if b < 0:
+        result = 1 / result
+    return result
