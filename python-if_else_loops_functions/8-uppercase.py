78feba6ef41d8ebdf21161a35797acfc899aaff8
MaFlet <mfletcher2808@hotmail.com>
2024-09-09 12:28:26 +1000

    Task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,16 +5,17 @@ a new line.
 """
 # pylint: disable=invalid-name
 # pylint: disable=consider-using-f-string
-
+# pylint: disable=redefined-builtin
 
 def uppercase(str):
     """
     Function that prints uppercase characters.
     """
-    new_string = ''
+    new_string = ""
     for char in str:
         if 'a' <= char <= 'z':
             new_string += chr(ord(char) - 32)
         else:
             new_string += char
-    print("{}".format(new_string))
+    print("{}".format(new_string), end="")
+    print()
