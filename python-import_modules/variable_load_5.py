de3a80579214d2e9b8c633b5b84c6026626a67bd
MaFlet <mfletcher2808@hotmail.com>
2024-09-09 19:13:43 +1000

    removing Task 4 files

--- python-import_modules/variable_load_5.py
+++ python-import_modules/variable_load_5.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This script will be imported to file
-variable_load_5.y that can prints its value
+5-variable_load.y that can prints its value
 """
 a = 98
 """Simple variable
