6a7b9b964c4958152833f0cf5fee089c25a05461
MaFlet <mfletcher2808@hotmail.com>
2024-09-09 23:56:00 +1000

    0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -5,6 +5,8 @@ Script that prints all integers of a list
 # pylint: disable=invalid-name
 # pylint: disable=consider-using-f-string
 # pylint: disable=dangerous-default-value
+
+
 def print_list_integer(my_list=[]):
     """Function to print list of all integers"""
     for number in my_list:
