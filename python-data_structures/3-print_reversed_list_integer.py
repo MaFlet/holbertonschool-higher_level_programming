e450de267dc6cda3300a545745204fbd2c4f3f26
MaFlet <mfletcher2808@hotmail.com>
2024-09-10 20:21:00 +1000

    Task 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that prints all integers of a list, in
+reverse order
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers in reverse order"""
+    if my_list:
+        for number in range(len(my_list) -1, -1, -1):
+            print("{:d}".format(my_list[number]))
