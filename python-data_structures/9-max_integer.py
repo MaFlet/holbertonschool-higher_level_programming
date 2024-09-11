637d0b3e76cec8c5915ae5d988e6cd70813b3c56
MaFlet <mfletcher2808@hotmail.com>
2024-09-11 21:38:01 +1000

    Task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that finds the biggest integer of a list
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def max_integer(my_list=[]):
+    """Search biggest integer of list"""
+    if not my_list:
+        return (0, None)
+    biggest = my_list[0]
+    for value in my_list:
+        if value > biggest:
+            biggest = value
+    return biggest
