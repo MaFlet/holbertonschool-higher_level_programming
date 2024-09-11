1f1056d852e8bd22a463b4640675641991197223
MaFlet <mfletcher2808@hotmail.com>
2024-09-11 22:15:44 +1000

    12-switch.py

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-Script that switch value of a and b
-"""
-# pylint: disable=invalid-name
-# pylint: disable=consider-using-f-string
-# pylint: disable=dangerous-default-value
 a = 89
 b = 10
 a, b = b, a
