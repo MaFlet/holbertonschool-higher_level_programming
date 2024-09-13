23ab221240ade29f5ffd404313898978ab51c4e1
MaFlet <mfletcher2808@hotmail.com>
2024-09-13 23:39:41 +1000

    Task 5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Script that returns the number of keys in a
+dictionary
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def number_keys(a_dictionary):
+    """Function that determine number of keys"""
+    return len(a_dictionary)
