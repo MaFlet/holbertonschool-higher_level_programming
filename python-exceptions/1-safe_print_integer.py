46ce6d5249e53183c27065b4d47c7afe24f51279
MaFlet <mfletcher2808@hotmail.com>
2024-09-14 18:09:00 +1000

    Task 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that prints an integer with "{:d}.format()"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def safe_print_integer(value):
+    """Prints an integer with the format string"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
