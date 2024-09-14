74c3af907021d6e1840dc858eba0f8c0836d214f
MaFlet <mfletcher2808@hotmail.com>
2024-09-14 18:23:32 +1000

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -20,5 +20,6 @@ def safe_print_list_integers(my_list=[], x=0):
     except IndexError:
         import traceback
         traceback.print_exc()
-    print()
+    finally:
+        print()
     return count
