740cb08d10099a657b8540af5e492a46cd1dd1b5
MaFlet <mfletcher2808@hotmail.com>
2024-09-14 18:19:40 +1000

    Task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script the prints the first x elements of a list and
+only integers
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Prints only integers of the list"""
+    count = 0
+    try:
+        for i in range(x):
+            element = my_list[i]
+            if isinstance(element, int):
+                print("{:d}".format(element), end="")
+                count += 1
+    except IndexError:
+        pass
+    print()
+    return count
