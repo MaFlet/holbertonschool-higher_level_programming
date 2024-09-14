098b4dd5f0ce14667c1ed41398de488d2b2b389f
MaFlet <mfletcher2808@hotmail.com>
2024-09-14 14:36:36 +1000

    Task 7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that replaces or adds key/value in a 
+dictionary
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Function replacing and adding key/value"""
+    a_dictionary[key] = value
+    return a_dictionary
