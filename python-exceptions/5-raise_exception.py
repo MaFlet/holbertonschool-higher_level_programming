ab250c853bdace40875b574c2723e92b76798499
MaFlet <mfletcher2808@hotmail.com>
2024-09-14 20:21:59 +1000

    Task 5

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Script that raises a type exception
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+def raise_exception():
+    """Raises exception type"""
+    raise TypeError
