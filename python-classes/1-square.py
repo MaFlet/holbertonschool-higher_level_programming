a7dd9ea29b380a1a445dce34a92401aea0c6bc66
MaFlet <mfletcher2808@hotmail.com>
2024-09-17 20:26:52 +1000

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# pylint: disable=invalid-name
+"""Script for defining a square"""
+
+
+class Square:
+    """Defining a square"""
+    def __init__(self, value):
+        self.__size = value
