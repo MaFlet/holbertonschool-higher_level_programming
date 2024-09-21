3634941ef07dc442989d62253f18f40b550f2852
MaFlet <mfletcher2808@hotmail.com>
2024-09-22 09:01:18 +1000

    Task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,8 +6,8 @@
 class Square:
     """Accessing and updating private attribute"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
 
     def area(self):
