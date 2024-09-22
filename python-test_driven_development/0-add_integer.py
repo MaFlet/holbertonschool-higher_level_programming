ad5ecef64cbd432c6e79f73fae2626d03614fc85
MaFlet <mfletcher2808@hotmail.com>
2024-09-22 18:19:45 +1000

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,9 +7,9 @@
 
 def add_integer(a, b=98):
     """Adding 2 integers"""
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     if a > 1e308 or b > 1e308:
         raise OverflowError("number too large")
