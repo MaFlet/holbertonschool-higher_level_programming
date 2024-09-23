850601ef2dccf361a6653813ef3240cc046beb81
MaFlet <mfletcher2808@hotmail.com>
2024-09-23 22:36:45 +1000

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Script that write an empty class BaseGeometry
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+class BaseGeometry:
+    """Class for BaseGeometry"""
+    def area(self):
+        raise Exception ("area() is not implemented")
