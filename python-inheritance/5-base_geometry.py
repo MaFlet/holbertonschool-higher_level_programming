e350df106ddd845d08133224aca3996bcae753b7
MaFlet <mfletcher2808@hotmail.com>
2024-09-23 22:27:35 +1000

    Task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Script that write an empty class BaseGeometry
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+class BaseGeometry:
+    """Empty class"""
