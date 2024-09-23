41f71021368033ac9ffe296f63b01fbe0cfd995d
MaFlet <mfletcher2808@hotmail.com>
2024-09-23 20:29:50 +1000

    Task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Tester when Square clas inherits from
+Rectangle with size attribute
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+def lookup(obj):
+    """Returns list of objects"""
+    return dir(obj)
