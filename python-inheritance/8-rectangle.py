3ebabef4f6cd1d9a634499c96581b3ebc571622e
MaFlet <mfletcher2808@hotmail.com>
2024-09-24 21:09:05 +1000

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,18 +6,7 @@ in BaseGeometry
 # pylint: disable=invalid-name
 # pylint: disable=consider-using-f-string
 # pylint: disable=dangerous-default-value
-
-
-class BaseGeometry:
-    """Class for BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -25,15 +14,6 @@ class Rectangle(BaseGeometry):
     in BaseGeometry"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-
-if __name__ == "__main__":
-    print(issubclass(Rectangle, BaseGeometry))
-    rect = Rectangle(3, 4)
-    print(rect.area())
