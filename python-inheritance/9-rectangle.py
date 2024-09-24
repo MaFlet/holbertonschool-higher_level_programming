a36a19bd8a911e56baa738c06d16135d9f605c0b
MaFlet <mfletcher2808@hotmail.com>
2024-09-24 21:19:02 +1000

    Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that write the class Rectangle inherited
+in BaseGeometry
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class for Rectangle that is inherited
+    in BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
