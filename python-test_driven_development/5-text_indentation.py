b2784c5e4e4ed0026867096dc05f4de5a9db944a
MaFlet <mfletcher2808@hotmail.com>
2024-09-26 16:28:42 +1000

    Task 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,11 +14,14 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     result = ""
     i = 0
+
     while i < len(text):
         result += text[i]
+
         if text[i] in {'.', '?', ':'}:
             result += "\n\n"
             while i + 1 < len(text) and text[i + 1] == ' ':
                 i += 1
+                
         i += 1
     print(result.strip())
