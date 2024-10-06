2c85665c0be843f8896f6b40f56d202128b13ba0
MaFlet <mfletcher2808@hotmail.com>
2024-10-06 16:18:22 +1100

    Task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that writes class Student that defines a
+student
+"""
+# pylint: disable=invalid-name
+# pylint: disable=consider-using-f-string
+# pylint: disable=dangerous-default-value
+
+
+class Student:
+    """
+    Represent a student with 3 public attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """
+        Returns dictionary representation of instance, with
+        attributes to be serialized into JSON format
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
