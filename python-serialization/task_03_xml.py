df5f8a3ab1104831dd59c81bcba3539483009c4d
MaFlet <mfletcher2808@hotmail.com>
2024-10-06 22:12:38 +1100

    Task 3

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -12,12 +12,10 @@ import xml.etree.ElementTree as ET
 def serialize_to_xml(dictionary, filename):
     root = ET.Element("data")
     for key, value in dictionary.items():
-        child = ET.SubElement(root, "item")
-        key_elem = ET.SubElement(child, "Key")
-        key_elem.text = str(key)
-        value_elem = ET.SubElement(child, "value")
-        value_elem.text = str(value)
-        value_elem.set("type", type(value).__name__)
+        item = ET.SubElement(root, "item")
+        item.set("Key", str(key))
+        item.text = str(value)
+        item.set("type", type(value).__name__)
 
     tree = ET.ElementTree(root)
     tree.write(filename)
@@ -27,10 +25,9 @@ def deserialize_from_xml(filename):
     root = tree.getroot()
     result = {}
     for item in root.findall("item"):
-        key = item.find("key").text
-        value_elem = item.find("value")
-        value = value_elem.text
-        value_type = value_elem.get("type")
+        key = item.get("Key")
+        value = item.text
+        value_type = item.get("type")
 
         if value_type == "int":
             value = int(value)
