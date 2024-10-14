0d0133573ccf354af6a0cf0f40d8b6a95b6de1f5
MaFlet <mfletcher2808@hotmail.com>
2024-10-14 23:51:48 +1100

    Task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,7 +5,12 @@ import json
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path == '/data':
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!.')
+        elif self.path == '/data':
             data = {
                 "name": "John",
                 "age": 30,
@@ -15,6 +20,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'{"status: "OK"}')
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
