9f44e1d119bbb6d23c7568a07b9c6e2cfccd2e54
MaFlet <mfletcher2808@hotmail.com>
2024-10-22 15:12:12 +1100

    Task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- Prints following description of the table first_table fromt the database hbtn_0c_0 in MySQL server
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'first_table'
-ORDER BY ORDINAL_POSITION;
+SHOW CREATE TABLE first_table;
