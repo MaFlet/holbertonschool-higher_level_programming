7d2867a781caf127f5683b4cb00608267d33450d
MaFlet <mfletcher2808@hotmail.com>
2024-10-31 21:39:19 +1100

    Task 13

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
--- Lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in mySQL server.
+-- Updates the score of Bob to 10 in the table second_table.
 UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
