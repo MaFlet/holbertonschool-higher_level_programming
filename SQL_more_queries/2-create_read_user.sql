b58ebbc6c2e4e748c2e49bb13a735da1b6d26511
MaFlet <mfletcher2808@hotmail.com>
2024-10-31 23:04:33 +1100

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- Creates the database hbtn_0d_2 and user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'hbtn_0d_2';
 FLUSH PRIVILEGES;
