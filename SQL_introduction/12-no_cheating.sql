89ffe70da290fc865f8825db08b25a2128747722
MaFlet <mfletcher2808@hotmail.com>
2024-10-31 20:48:52 +1100

    Task 12

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
 -- Lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in mySQL server.
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+UPDATE second_table
+SET name='Bob'
+WHERE score='10';
