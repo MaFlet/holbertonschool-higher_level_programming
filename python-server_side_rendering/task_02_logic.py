4c41e7a0fe36cf50cc7497e4df6f2f3028e3c5f9
MaFlet <mfletcher2808@hotmail.com>
2024-11-17 09:52:57 +1100

    Task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import json
-import os
 
 app = Flask(__name__)
 
@@ -19,14 +18,11 @@ def contact():
 @app.route('/items')
 def items():
     try:
-        if os.path.exists('items.json'):
-            with open('items.json', 'r') as file:
-                rows = json.load(file)
-                return render_template('items.html', items= rows['items'])
-        else:
-            return render_template('items.html', items=[])
-    except (json.JSONDecodeError, IOError):
-        return render_template('items.html', items=[])
+        with open('items.json', 'r') as file:
+            rows = json.load(file)
+            return render_template('template/items.html', items= rows.get['items', []])
+    except (FileNotFoundError, json.JSONDecodeError, KeyError):
+        return render_template('template/items.html', items=[])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
