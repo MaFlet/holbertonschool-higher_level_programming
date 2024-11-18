13dcdd82f626f59ab75b18d78877e85f3e2ef57e
MaFlet <mfletcher2808@hotmail.com>
2024-11-18 20:26:19 +1100

    Task 0

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,5 +1,9 @@
-const header = document.querySelector("header");
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector("header")
 
-function updateColor() {
-    header.style.color = "#FF0000";
-}
+    function updateColor() {
+        header.style.color = "#FF0000"
+    }
+
+    updateColor();
+});
