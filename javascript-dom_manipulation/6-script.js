73750b5540d8ac58b70d06bb1af63c0a2f5f78b2
MaFlet <mfletcher2808@hotmail.com>
2024-11-19 09:06:18 +1100

    Task 6

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,9 +1,9 @@
 fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
     .then(function(response) {
-        return response.json();
+        return response.json(); // converts code into JSON, javascript friendly format
     })
-    .then(function(data) {
-        document.querySelector("#character").textContent = data.name;
+    .then(function(data) { // function passes "data" parameter
+        document.querySelector("#character").textContent = data.name; // search for HTML tag with ID character and then it will be stored "data.name"
     })
     .catch(function(error) {
         console.log(error);
