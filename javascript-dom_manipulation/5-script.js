834fc655c27f4f5b91f09f2b45cfba08d0f5a971
MaFlet <mfletcher2808@hotmail.com>
2024-11-19 09:09:21 +1100

    Task 6

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () { // wait till the webpage is fully loaded "DOMContendLoaded"
     const header = document.querySelector("header");
-    const updateHeader = document.querySelector("#update_header")
+    const updateHeader = document.querySelector("#update_header") // search for two HTML elements - with ID "update_header" and <header>
   
       updateHeader.addEventListener('click', function () {
-      header.textContent = "New Header!!!";
+      header.textContent = "New Header!!!"; // once user click on update the header heading it will change  to New Header !
     });
   });
