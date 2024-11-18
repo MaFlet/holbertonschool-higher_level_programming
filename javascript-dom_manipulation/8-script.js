e24d7554bdb21c852be4c8a33b6f6846e32a1fb3
MaFlet <mfletcher2808@hotmail.com>
2024-11-19 08:44:55 +1100

    Task 8

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,10 +1,10 @@
 fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
     .then(function(response) {
-        return response.json();
+        return response.json(); // Once website response, the code converts to readable javascript format(JSON)
     })
     .then(function(data) {
-        document.querySelector("#hello").textContent = data.hello;
+        document.querySelector("#hello").textContent = data.hello; // the hello french language will be stored in data.hello
     })
     .catch(function(error) {
-        console.log(error);
+        console.log(error); // if anything goes wrong during the process it will display a message on a console
     });
